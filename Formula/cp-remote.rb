fb85a01e5c97f712c38df435d27a32c7c169c945
alessandrozucca <alessandrozucca@gmail.com>
2017-04-20 17:22:54 +0100

    updating host from continuouspipe.github.io to aws s3 bucket url

--- Formula/cp-remote.rb
+++ Formula/cp-remote.rb
@@ -2,7 +2,7 @@ class CpRemote < Formula
   desc "Tool to create and sync code to a Continuous Pipe remote environment"
   homepage "https://docs.continuouspipe.io/remote-development/getting-started/"
   version '0.1.2'
-  url "https://continuouspipe.github.io/remote-environment-client/#{version}/darwin-amd64.gz"
+  url "https://inviqa-cp-remote-client-environment.s3-eu-west-1.amazonaws.com/downloads/#{version}/darwin-amd64.gz"
   sha256 "6aea96585c87256981b406cbe151d511a4387f0814ba0330f9724be971b90077"
 
   def install
