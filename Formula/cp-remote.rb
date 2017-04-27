962b3160e29748b019efc96b7883ffe5b1f2b754
alessandrozucca <alessandrozucca@gmail.com>
2017-04-27 17:41:58 +0100

    Release version 0.1.3

--- Formula/cp-remote.rb
+++ Formula/cp-remote.rb
@@ -1,9 +1,9 @@
 class CpRemote < Formula
   desc "Tool to create and sync code to a Continuous Pipe remote environment"
   homepage "https://docs.continuouspipe.io/remote-development/getting-started/"
-  version '0.1.2'
+  version '0.1.3'
   url "https://inviqa-cp-remote-client-environment.s3-eu-west-1.amazonaws.com/downloads/#{version}/darwin-amd64.gz"
-  sha256 "6aea96585c87256981b406cbe151d511a4387f0814ba0330f9724be971b90077"
+  sha256 "325dd9e6e3023ddb99bb9499308c6a7d0d4446afa71717a7c3cee173c95d5709"
 
   def install
     system "mv darwin-amd64 cp-remote"
